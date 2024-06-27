fe50c28bd00d2dc755575cfb998747716bb83b40
Bernard Bolter <bernardbolter@gmail.com>
2024-06-27 16:01:11 +0200

    logo update

--- src/app/[lng]/page.js
+++ src/app/[lng]/page.js
@@ -1,67 +1,78 @@
+import { useContext } from 'react'
+import { HistoryContext } from '@/providers/HistoryProvider'
 import { useTranslation } from '../i18n'
 
 import Artworks from '@/components/Artworks'
 import Nav from '@/components/Nav'
 import Logo from '@/components/Logo'
 
+async function getArtwork() {
+  const query =  `
+    {
+      allArtwork(first: 1000, where: {categoryName: "A Colorful History"}) {
+        nodes {
+          slug
+          artworkFields {
+            slug
+            city
+            artworklink {
+              url
+              title
+            }
+            country
+            forsale
+            height
+            lat
+            lng
+            medium
+            metadescription
+            metakeywords
+            orientation
+            proportion
+            series
+            size
+            style
+            width
+            year
+          }
+          title(format: RENDERED)
+          databaseId
+          id
+        }
+      }
+    }
+  `
+
+  const res = await fetch("https://digitalcityseries.com/bolter/graphql", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ query }),
+    next: {next: { revalidate: 10 }}
+  })
+
+  const data = await res.json()
+
+  return data.data.allArtwork.nodes
+}
+
 
 const Home = async ({ params: { lng} }) => {
   const { t } = await useTranslation(lng, 'common')
-  // const { data } = await fetch(process.env.NEXT_PUBLIC_WORDPRESS_BASE_URL, {
-  //   method: "POST",
-  //   headers: {
-  //     "Content-Type": "application/json",
-  //   },
-  //   body: JSON.stringify({
-  //     // query: `
-  //     //   query ColorQuery {
-  //     //     allArtwork(first: 1000, where: {categoryName: "A Colorful History"}) {
-  //     //       nodes {
-  //     //         slug
-  //     //         artworkFields {
-  //     //           slug
-  //     //           city
-  //     //           artworklink {
-  //     //             url
-  //     //             title
-  //     //           }
-  //     //           country
-  //     //           forsale
-  //     //           height
-  //     //           lat
-  //     //           lng
-  //     //           medium
-  //     //           metadescription
-  //     //           metakeywords
-  //     //           orientation
-  //     //           proportion
-  //     //           series
-  //     //           size
-  //     //           style
-  //     //           width
-  //     //           year
-  //     //         }
-  //     //         title(format: RENDERED)
-  //     //         databaseId
-  //     //         id
-  //     //       }
-  //     //     }
-  //     //   }
-  //     // `,
-  //   }),
-  //   next: { revalidate: 10 },
-  // }).then((res) => res.json());
-
-  // let artworks = data.allArtwork?.nodes
+  const [history, setHistory] = useContext(HistoryContext)
+  const artwork = await getArtwork()
+  console.log("art:", artwork)
+  setHistory(state => ({ ...state, }))
 
   return (
     <section className="home-container">
-      {/* <Nav lng={lng} /> */}
+      <Nav lng={lng} />
       <Logo lng={lng} />
-      {/* <Artworks 
-        artworks={[]}
+      <Artworks 
+        artworks={artwork}
         lng={lng}  
-      /> */}
+      />
     </section>
   )
 }
